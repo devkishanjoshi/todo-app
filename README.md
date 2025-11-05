fcea398dbbac853c34f2711c20bc1d11342939c8
Devkishan Joshi <devkishanjoshi@Devkishans-MacBook-Air.local>
2025-11-05 11:16:01 +0530

    todo-app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # todo-app
 Python To-Do App is a lightweight and easy-to-use task management tool built using Python. It allows users to add, update, delete, and mark tasks as complete. The project is designed for beginners to learn CRUD operations, file handling, and basic Python application structure — and can easily be extended with a database or web framework.
+# Define Features 
+
+- Define Features 
+- Project Structure
+- App Setup
+- Models 
+- Routes 
+- HTML Templates Design 
+- CSS Design 
+- Database init 
+
---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+# Create DB Globally 
+db = SQLAlchemy() 
+
+
+def create_app(): 
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.__init__(app) # Link DB Object with app
+
+    from app.routes.auth import auth_bp
+    from app.routes.tasks import tasks_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tasks_bp)
+
+    return app
